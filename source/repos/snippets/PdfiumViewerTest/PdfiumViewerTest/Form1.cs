efca07b6c7cc765f8415d927924a3da5d0121199
Jan Wagemakers <jan@debianI7.thuis>
2018-02-22 21:25:09 +0100

    pdfiumviewer snippet

---
+++ source/repos/snippets/PdfiumViewerTest/PdfiumViewerTest/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Printing;
+using System.Windows.Forms;
+
+namespace PdfiumViewerTest
+{
+    public partial class Form1 : Form
+    {
+        private string prn;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            
+                PrinterSettings settings = new PrinterSettings();
+                foreach (string printer in PrinterSettings.InstalledPrinters)
+                {
+                    Console.WriteLine(printer);
+                    settings.PrinterName = printer;
+                    if (settings.IsDefaultPrinter) {
+                        Console.WriteLine("Default = " + printer);
+                        prn = printer;
+                    }
+            }
+                
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PrintPDF p = new PrintPDF();
+            if (p.Print(prn, "a4", "C:/Users/Jan Wagemakers/Documents/MEGA/2017-2018/ProjectWerk/DesignDocument/1.1_korteOmschrijving/korteOmschrijving_Wagemakers_Jan_v20171101.pdf", 1))
+            {
+                MessageBox.Show("OK");
+            } else
+            {
+                MessageBox.Show("FAIL");
+            }
+        }
+    }
+}
---
+++ source/repos/snippets/PdfiumViewerTest/PdfiumViewerTest/PrintPDF.cs
@@ -0,0 +1,60 @@
+﻿using PdfiumViewer;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdfiumViewerTest
+{
+    class PrintPDF {
+
+        public bool Print(string printer, string paperName, string filename, int copies)
+        {
+            try
+            {
+                var printerSettings = new PrinterSettings
+                {
+                    PrinterName = printer,
+                    Copies = (short)copies,
+                };
+
+                Console.WriteLine("printerSettings DONE");
+
+                var pageSettings = new PageSettings(printerSettings)
+                {
+                    Margins = new Margins(0, 0, 0, 0),
+                };
+
+                Console.WriteLine("pageSettings DONE");
+
+                foreach (PaperSize paperSize in printerSettings.PaperSizes)
+                {
+                    if(paperSize.PaperName == paperName)
+                    {
+                        pageSettings.PaperSize = paperSize;
+                        break;
+                    }
+                }
+
+                Console.WriteLine("paperSize DONE");
+
+                using (var document = PdfDocument.Load(filename))
+                {
+                    using(var printDocument = document.CreatePrintDocument())
+                    {
+                        printDocument.PrinterSettings = printerSettings;
+                        printDocument.DefaultPageSettings = pageSettings;
+                        printDocument.PrintController = new StandardPrintController();
+                        printDocument.Print();
+                    }
+                }
+                return true;
+            } catch 
+            {
+                return false;
+            }
+        }
+    }
+}
